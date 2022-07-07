868b6fce2651a08ae0fb08534b80fc4d0693cecd
joaoelias1921 <joao.reis@aluno.unc.br>
2022-07-07 14:42:50 -0300

    Commit geral, fim do curso

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -9,37 +9,44 @@ import {
   InputLabel,
   InputAdornment 
 } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+import { UsuarioContext } from 'common/context/Usuario';
+import { useContext } from 'react';
 
 function Login() {
+  const history = useHistory();
+  const { nome, setNome, saldo, setSaldo } = useContext(UsuarioContext);
   return (
     <Container>
       <Titulo>
         Insira o seu nome
       </Titulo>
       <InputContainer>
-        <InputLabel>
-          Nome
-        </InputLabel>
-        <Input
+        <InputLabel>Nome</InputLabel>
+        <Input 
+          value={nome} 
+          onChange={(event) => setNome(event.target.value)}
           type="text"
         />
       </InputContainer>
       <InputContainer>
-        <InputLabel>
-          Saldo
-        </InputLabel>
+        <InputLabel>Saldo</InputLabel>
         <Input
-        type="number"
-        startAdornment={
-          <InputAdornment position="start">
-            R$
-          </InputAdornment>
-        }
-      />
+          type="number"
+          value={saldo}
+          onChange={(event) => setSaldo(event.target.value)}
+          startAdornment={
+            <InputAdornment position="start">
+              R$
+            </InputAdornment>
+          }
+        />
       </InputContainer>
-      <Button
-        variant="contained"
+      <Button 
+        variant="contained" 
         color="primary"
+        disabled={nome.length < 3}
+        onClick={() => history.push("/feira")}
       >
         Avançar
       </Button>
